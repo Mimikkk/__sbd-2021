047b8d5f6a50002a2cb7f1d062b186dcf127f9e9
Hououin Kyouma <inwazzin@gmail.com>
2021-10-20 02:15:41 +0200

    Revert "d.a.?"
    This reverts commit 89a3b1f9c742de1d953c062c7ef4a6acd60a563c.

--- src/Backend/Startup.cs
+++ src/Backend/Startup.cs
@@ -3,47 +3,51 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Backend
+namespace AspNetCoreDemoApp
 {
-public class Startup
-{
-  public void ConfigureServices(IServiceCollection services)
-  {
-    services
-      .AddHttpsRedirection(options => { options.HttpsPort = 443; })
-      .AddMvcCore()
-      .AddCors(options => {
-        options.AddPolicy("CorsPolicy",
-          builder => builder.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader());
-      });
-
-    services.Configure<ForwardedHeadersOptions>(options => {
-      options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-      options.KnownNetworks.Clear();
-      options.KnownProxies.Clear();
-    });
-  }
-
-  public void Configure(IApplicationBuilder app)
-  {
-    app.UseForwardedHeaders();
-
-    if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DYNO")))
+    public class Startup
     {
-      Console.WriteLine("Use https redirection");
-      app.UseHttpsRedirection();
-    }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddHttpsRedirection(options => { options.HttpsPort = 443; })
+                .AddMvcCore()
+                .AddCors(options =>
+                {
+                    options.AddPolicy("CorsPolicy",
+                        builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+                });
 
-    app
-      .UseRouting()
-      .UseDefaultFiles()
-      .UseStaticFiles()
-      .UseCors("CorsPolicy")
-      .UseEndpoints(endpoints => {
-        endpoints.MapDefaultControllerRoute();
-      });
-  }
-}
+            services.Configure<ForwardedHeadersOptions>(options =>
+            {
+                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor |
+                                           ForwardedHeaders.XForwardedProto;
+                options.KnownNetworks.Clear();
+                options.KnownProxies.Clear();
+            });
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseForwardedHeaders();
+
+            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DYNO")))
+            {
+                Console.WriteLine("Use https redirection");
+                app.UseHttpsRedirection();
+            }
+
+            app
+                .UseRouting()
+                .UseDefaultFiles()
+                .UseStaticFiles()
+                .UseCors("CorsPolicy")
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapDefaultControllerRoute();
+                });
+        }
+    }
 }
