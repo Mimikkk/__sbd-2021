ef5255046ce82a7c38344644b3586b269d04fa45
Hououin Kyouma <inwazzin@gmail.com>
2021-10-20 02:17:41 +0200

    abd

--- src/Backend/Startup.cs
+++ src/Backend/Startup.cs
@@ -3,51 +3,47 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace AspNetCoreDemoApp
+namespace Backend
 {
-    public class Startup
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
     {
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services
-                .AddHttpsRedirection(options => { options.HttpsPort = 443; })
-                .AddMvcCore()
-                .AddCors(options =>
-                {
-                    options.AddPolicy("CorsPolicy",
-                        builder => builder.AllowAnyOrigin()
+        services
+            .AddHttpsRedirection(options => { options.HttpsPort = 443; })
+            .AddMvcCore()
+            .AddCors(options => {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
                         .AllowAnyMethod()
                         .AllowAnyHeader());
-                });
-
-            services.Configure<ForwardedHeadersOptions>(options =>
-            {
-                options.ForwardedHeaders = ForwardedHeaders.XForwardedFor |
-                                           ForwardedHeaders.XForwardedProto;
-                options.KnownNetworks.Clear();
-                options.KnownProxies.Clear();
             });
-        }
 
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseForwardedHeaders();
-
-            if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DYNO")))
-            {
-                Console.WriteLine("Use https redirection");
-                app.UseHttpsRedirection();
-            }
-
-            app
-                .UseRouting()
-                .UseDefaultFiles()
-                .UseStaticFiles()
-                .UseCors("CorsPolicy")
-                .UseEndpoints(endpoints =>
-                {
-                    endpoints.MapDefaultControllerRoute();
-                });
-        }
+        services.Configure<ForwardedHeadersOptions>(options => {
+            options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+            options.KnownNetworks.Clear();
+            options.KnownProxies.Clear();
+        });
     }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseForwardedHeaders();
+
+        if (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("DYNO")))
+        {
+            Console.WriteLine("Use https redirection");
+            app.UseHttpsRedirection();
+        }
+
+        app
+            .UseRouting()
+            .UseDefaultFiles()
+            .UseStaticFiles()
+            .UseCors("CorsPolicy")
+            .UseEndpoints(endpoints => {
+                endpoints.MapDefaultControllerRoute();
+            });
+    }
+}
 }
