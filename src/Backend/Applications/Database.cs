51d9e5bfdceaa961bf6706e0fa2c095cdcf1349b
Hououin Kyouma <inwazzin@gmail.com>
2021-11-02 21:50:48 +0100

    Mac Is Love, I love Mac

--- src/Backend/Applications/Database.cs
+++ src/Backend/Applications/Database.cs
@@ -7,18 +7,21 @@ namespace Backend.Applications;
 
 public static partial class Application
 {
-  private static readonly string DatabaseUri = CalculateDatabaseUri();
+  private static string DatabaseUri => IsConnectionByInfo ? FromInfo() : FromUrl();
+
   public static readonly NpgsqlConnection Database = EstablishConnection();
 
   private static NpgsqlConnection EstablishConnection()
   {
     var connection = new NpgsqlConnection(DatabaseUri);
+    
     connection.Open();
     Console.WriteLine("Connected to the database!");
 
     return connection;
   }
-  private static string CalculateDatabaseUri()
+
+  private static string FromUrl()
   {
     var uri = new Uri(Environment.GetEnvironmentVariable("DATABASE_URL")!);
 
@@ -33,4 +36,23 @@ public static partial class Application
       Database = uri.LocalPath.TrimStart('/')
     }.ToString();
   }
+
+  private static string FromInfo()
+  {
+    var database = Environment
+      .GetEnvironmentVariable("DATABASE_URL")!
+      .Split(' ')
+      .Select(item => item.Split("="))
+      .ToDictionary(item => item[0], item => item[1]);
+
+    return new NpgsqlConnectionStringBuilder {
+      Host = database["host"],
+      Port = int.Parse(database["port"]),
+      Username = database["user"],
+      Password = database["password"],
+      Database = database["dbname"]
+    }.ToString();
+  }
+
+  private static bool IsConnectionByInfo => Environment.GetEnvironmentVariable("DATABASE_URL")!.StartsWith("dbname");
 }
