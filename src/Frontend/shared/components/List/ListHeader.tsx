89e755f01c62d77b22098c343817539ef890565f
Monika <63261009+monikazielinska0512@users.noreply.github.com>
2021-11-13 00:25:21 +0100

    buttonki i react-table nie czyta typow ale dziala XD 2

--- src/Frontend/shared/components/List/ListHeader.tsx
+++ src/Frontend/shared/components/List/ListHeader.tsx
@@ -1,21 +1,22 @@
 import { HeaderGroup } from 'react-table';
 
-
 export interface ListHeaderProps<T extends object> {
+  // @ts-ignore
   headerGroups: HeaderGroup<T>[];
 }
 
-export const ListHeader = <T extends object>({ headerGroups }: ListHeaderProps<T>) => {
+export const ListHeader = <T extends object>({
+  headerGroups,
+}: ListHeaderProps<T>) => {
   return (
     <thead>
-    {headerGroups.map(headerGroup => (
-      <tr {...headerGroup.getHeaderGroupProps()}>
-        {headerGroup.headers.map(column => (
-          <th {...column.getHeaderProps()}>
-            {column.render('Header')}
-          </th>
-        ))}
-      </tr>
-    ))}
-    </thead>);
+      {headerGroups.map((headerGroup) => (
+        <tr {...headerGroup.getHeaderGroupProps()}>
+          {headerGroup.headers.map((column) => (
+            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+          ))}
+        </tr>
+      ))}
+    </thead>
+  );
 };
