4972bc20ae7f812cb4c6cdc7917fb74674f7cea0
Hououin Kyouma <inwazzin@gmail.com>
2021-11-12 17:07:03 +0100

    css

--- src/Frontend/shared/components/Navigator/Navigator.tsx
+++ src/Frontend/shared/components/Navigator/Navigator.tsx
@@ -5,7 +5,7 @@ import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
 import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
 import { Props as MenuProps } from './Menu';
 import { useFactory } from 'shared/hooks';
-import style from './Navigator.module.scss';
+import { Navigator as NavigatorStyle } from './Navigator.module.scss';
 
 const development: MenuProps[] = [
   {
@@ -57,7 +57,7 @@ export const Navigator: VFC = () => {
   const [Menus] = useFactory(Menu);
 
   return (
-    <Drawer className={style.Navigator} variant="permanent" open>
+    <Drawer className={NavigatorStyle} variant="permanent" open>
       <Menus items={menus} />
     </Drawer>
   );
