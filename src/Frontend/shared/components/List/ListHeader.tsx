d3a4fe78bb31526673941e228bd30f5e8c5749ee
Monika <63261009+monikazielinska0512@users.noreply.github.com>
2021-11-12 17:17:14 +0100

    si es es

--- src/Frontend/shared/components/List/ListHeader.tsx
+++ src/Frontend/shared/components/List/ListHeader.tsx
@@ -1,22 +1,19 @@
 import { HeaderGroup } from 'react-table';
 import React from 'react';
-
+import {listHeader as listHeaderStyle} from './ListHeader.module.scss'
 export interface ListHeaderProps<T extends object> {
   headerGroups: HeaderGroup<T>[];
 }
 
 export const ListHeader = <T extends object>({ headerGroups }: ListHeaderProps<T>) => {
   return (
-    <thead>
+    <thead className={listHeaderStyle}>
     {headerGroups.map(headerGroup => (
       <tr {...headerGroup.getHeaderGroupProps()}>
         {headerGroup.headers.map(column => (
-          <th {...column.getHeaderProps()} style={{
-            borderBottom: 'solid 2px rgba(227, 229, 229, 1)',
-          }}>
+          <th {...column.getHeaderProps()}>
             {column.render('Header')}
           </th>
-
         ))}
       </tr>
     ))}
