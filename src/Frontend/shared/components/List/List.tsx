ffa50e1b1d0b3d73fac58cac92d28c1c8447d0ee
Monika <63261009+monikazielinska0512@users.noreply.github.com>
2021-11-12 17:26:07 +0100

    si es es 2

--- src/Frontend/shared/components/List/List.tsx
+++ src/Frontend/shared/components/List/List.tsx
@@ -2,6 +2,7 @@ import { Column, useTable } from 'react-table';
 import React from 'react';
 import { ListHeader } from 'shared/components/List/ListHeader';
 import { ListBody } from 'shared/components/List/ListBody';
+import {List as ListStyle} from 'shared/components/List/List.module.scss'
 
 export interface Props <T extends object> {
   columns: Column<T>[];
@@ -18,7 +19,7 @@ export const List = <T extends object> ({columns, items}: Props<T>) => {
   } = useTable({ columns, data: items})
 
   return (
-    <table {...getTableProps()} style={{width: '100%', borderCollapse: 'collapse'}}>
+    <table {...getTableProps()} className={ListStyle}>
       <ListHeader headerGroups={headerGroups}/>
       <ListBody rows={rows} prepareRow={prepareRow} getTableBodyProps={getTableBodyProps}/>
     </table>
--- src/Frontend/shared/components/List/ListHeader.tsx
+++ src/Frontend/shared/components/List/ListHeader.tsx
@@ -1,13 +1,12 @@
 import { HeaderGroup } from 'react-table';
-import React from 'react';
-import {listHeader as listHeaderStyle} from './ListHeader.module.scss'
+
 export interface ListHeaderProps<T extends object> {
   headerGroups: HeaderGroup<T>[];
 }
 
 export const ListHeader = <T extends object>({ headerGroups }: ListHeaderProps<T>) => {
   return (
-    <thead className={listHeaderStyle}>
+    <thead>
     {headerGroups.map(headerGroup => (
       <tr {...headerGroup.getHeaderGroupProps()}>
         {headerGroup.headers.map(column => (
