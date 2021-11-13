146e4ba1b0a30210b0eda67c69b53d0cf9a1e42f
Hououin Kyouma <inwazzin@gmail.com>
2021-11-13 23:18:17 +0100

    styles

--- src/Frontend/styles/style.ts
+++ src/Frontend/styles/style.ts
@@ -2,8 +2,9 @@ import { merge } from 'lodash';
 import list from './List.module.scss';
 import navigator from './Navigator.module.scss';
 import scheduler from './Scheduler.module.scss';
+import dateSelect from './DateSelect.module.scss';
 
-const styles = merge(list, navigator, scheduler);
+const styles = merge(list, navigator, scheduler, dateSelect);
 
 export const style = (key: string) => {
   if (!styles[key]) {
