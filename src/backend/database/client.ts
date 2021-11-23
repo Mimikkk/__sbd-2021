9eaa0000afbcf03dd89824a4caa026e1bd88d0fb
Hououin Kyouma <inwazzin@gmail.com>
2021-11-23 23:15:29 +0100

    obrzydliwy fix

--- src/backend/database/client.ts
+++ src/backend/database/client.ts
@@ -2,9 +2,13 @@ import { PrismaClient } from "@prisma/client";
 
 const global_: any = global;
 
-// Specjalnie takie brzydkie ;)
-const prisma: PrismaClient =
-  (process.env.NODE_ENV === "production" && new PrismaClient()) ||
-  (!global_.prisma && new PrismaClient(), global_.prisma);
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global_.prisma) global_.prisma = new PrismaClient();
+  prisma = global_.prisma;
+}
 
 export { prisma as client };
