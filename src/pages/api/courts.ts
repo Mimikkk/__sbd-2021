c16f8ed097818de6a4fe00419fd4a23d40a8e01b
Hououin Kyouma <inwazzin@gmail.com>
2021-11-23 10:53:20 +0100

    select

--- src/pages/api/courts.ts
+++ src/pages/api/courts.ts
@@ -3,19 +3,12 @@ import { selectWith } from "$sql";
 import { translateCourt } from "$sql/orm";
 
 const handler = async (request: NextApiRequest, response: NextApiResponse) => {
-  const items = await selectWith(translateCourt)`
-      select *
-      from courts
-  `;
+  const items = await selectWith(translateCourt)`select * from courts`;
 
   if (Array.isArray(items)) {
     await response.status(200).json({
       items,
       total: items.length,
-    });
-  } else {
-    await response.status(500).json({
-      message: "Failed to fetch items",
     });
   }
 };
