2c32a06b5b317ab48612478ed2fe9e4b6baf8c73
Hououin Kyouma <inwazzin@gmail.com>
2021-11-23 20:22:47 +0100

    update trigger

---
+++ src/backend/database/scripts/triggers/update.sql
@@ -0,0 +1,71 @@
+drop function if exists footprint_update;
+create or replace function footprint_update() returns trigger
+as $$
+begin
+  raise notice '% %', new.id, old.id;
+  new.updated_at := current_timestamp;
+  return new;
+end;
+$$ language plpgsql;
+
+drop trigger if exists footprint_update on footprint;
+drop trigger if exists footprint_update on items;
+drop trigger if exists footprint_update on courts;
+drop trigger if exists footprint_update on discounts;
+drop trigger if exists footprint_update on prices;
+drop trigger if exists footprint_update on person;
+drop trigger if exists footprint_update on clients;
+drop trigger if exists footprint_update on employees;
+drop trigger if exists footprint_update on reservation;
+drop trigger if exists footprint_update on item_reservations;
+drop trigger if exists footprint_update on court_reservations;
+create trigger footprint_update
+  before update
+  on footprint
+execute procedure footprint_update();
+create trigger footprint_update
+  before update
+  on items
+execute procedure footprint_update();
+create trigger footprint_update
+  before update
+  on courts
+execute procedure footprint_update();
+create trigger footprint_update
+  before update
+  on discounts
+execute procedure footprint_update();
+create trigger footprint_update
+  before update
+  on prices
+execute procedure footprint_update();
+create trigger footprint_update
+  before update
+  on person
+execute procedure footprint_update();
+create trigger footprint_update
+  before update
+  on clients
+execute procedure footprint_update();
+create trigger footprint_update
+  before update
+  on employees
+execute procedure footprint_update();
+create trigger footprint_update
+  before update
+  on reservation
+execute procedure footprint_update();
+create trigger footprint_update
+  before update
+  on item_reservations
+execute procedure footprint_update();
+create trigger footprint_update
+  before update
+  on court_reservations
+execute procedure footprint_update();
+
+select * from footprint;
+insert into courts(name, floor, is_covered) values ('zooo', 'Monia jest super', true);
+insert into courts(name, floor, is_covered) values ('Super', 'Super jest Monika', true);
+insert into courts(name, floor, is_covered) values ('Monika', 'Monika super jest', true);
+insert into courts(name, floor, is_covered) values ('Jest', 'Jest Mon super', true);
