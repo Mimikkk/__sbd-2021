13130a08e0dad46c2d3b68c06342bac74106f259
Hououin Kyouma <inwazzin@gmail.com>
2021-12-29 03:37:04 +0100

    update transaction constraint

--- src/backend/database/scripts/models/transaction.sql
+++ src/backend/database/scripts/models/transaction.sql
@@ -1,10 +1,18 @@
 drop table if exists transaction;
+drop function if exists reservation_fk_constraint;
+
+create function reservation_fk_constraint(v_id uuid) returns boolean as $$
+begin
+  return exists(select * from court_reservation cr where cr.id = v_id) or
+         exists(select * from item_reservation ir where ir.id = v_id);
+end;
+$$ language plpgsql;
 
 create table transaction
 (
   primary key (id),
   cost           udecimal not null,
-  reservation_id uuid     not null references reservation (id),
+  reservation_id uuid     not null check (reservation_fk_constraint(reservation_id)),
   client_id      uuid     not null references client (id),
   discount_id    uuid     null default null references discount (id)
 ) inherits (footprint);
