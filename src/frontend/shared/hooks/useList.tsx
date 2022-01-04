5f9c132a4d0627e174577a1918a94361f23f612e
Hououin Kyouma <inwazzin@gmail.com>
2022-01-04 08:54:26 +0100

    useList + useListFetch separation

--- src/frontend/shared/hooks/useList.tsx
+++ src/frontend/shared/hooks/useList.tsx
@@ -1,42 +1,10 @@
-import { BaseModel } from "@models";
-import { FC, useCallback, useEffect, useMemo, useState } from "react";
-import { flow } from "lodash";
-import {
-  listError,
-  listInitial,
-  listLoading,
-  listSuccess,
-  ListState,
-  ListResponse,
-} from "$/services/types";
-import { useToggle } from "shared/hooks";
-import { isLoading } from "shared/utils/requests";
-import { ListContext } from "shared/contexts";
 import { List, ListProps } from "shared/components/List";
-
-interface UseListProps<Item extends BaseModel> {
-  list: ListState<Item>;
-  refresh: () => void;
-}
-const useListFetch = <Item extends BaseModel, Params = undefined>(
-  fetch: (params?: Params) => Promise<ListResponse<Item>>,
-  dependencies: any[] = [],
-  params?: Params
-): UseListProps<Item> => {
-  const [shouldRefresh, refresh] = useToggle();
-  const [state, setState] = useState<ListState<Item>>(listInitial);
-
-  const handleError = useMemo(() => flow(listError, setState), []);
-  const handleSuccess = useMemo(() => flow(listSuccess, setState), []);
-  const handlePending = useMemo(() => flow(listLoading, setState), []);
-
-  useEffect(() => {
-    handlePending();
-    fetch(params).then(handleSuccess).catch(handleError);
-  }, [...dependencies, shouldRefresh]);
-
-  return { list: { ...state }, refresh } as const;
-};
+import { isLoading } from "shared/utils/requests";
+import { ListResponse } from "$/services/types";
+import { ListContext } from "shared/contexts";
+import { useListFetch } from "shared/hooks";
+import { FC, useCallback } from "react";
+import { BaseModel } from "@models";
 
 export const useList = <Item extends BaseModel, Params = undefined>(
   fetch: (params?: Params) => Promise<ListResponse<Item>>
