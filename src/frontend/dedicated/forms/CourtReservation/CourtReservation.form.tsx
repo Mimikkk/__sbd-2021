1dc35922128cfbe3ba187f800e99495d87ec628e
Hououin Kyouma <inwazzin@gmail.com>
2022-01-14 02:25:30 +0100

    format

--- src/frontend/dedicated/forms/CourtReservation/CourtReservation.form.tsx
+++ src/frontend/dedicated/forms/CourtReservation/CourtReservation.form.tsx
@@ -6,7 +6,7 @@ import {
   employeeService,
 } from "@services";
 import { courtReservationSchema } from "./CourtReservation.validation";
-import { isEntity } from "shared/utils";
+import { isEntity, personToOptions } from "shared/utils";
 import {
   DateSelect,
   Form,
@@ -16,9 +16,9 @@ import {
 import { useListContext } from "shared/contexts";
 import { useDate } from "shared/hooks";
 import React, { useEffect, useMemo, useState } from "react";
-import { style } from "styles";
 import { filter } from "lodash";
-import { itemsToOptions, personToOptions } from "shared/utils/options";
+import { itemsToOptions } from "shared/utils/options";
+import { addDays } from "date-fns";
 
 export const createReservationValues = <
   T extends CourtReservation.Model
@@ -61,7 +61,6 @@ export const CourtReservationForm = <T extends CourtReservation.Model>({
       .then(({ items }) => setTeachers(filter(items, "isTeacher")));
   }, []);
 
-  console.log({ date });
   return (
     <Form
       validationSchema={courtReservationSchema}
@@ -69,39 +68,38 @@ export const CourtReservationForm = <T extends CourtReservation.Model>({
       onSubmit={handleSuccess}
       onRemove={handleRemove}
     >
-      <div className={style("form")}>
-        <SelectField
-          name={"courtId"}
-          label={"Choose court"}
-          options={itemsToOptions(courts)}
-        />
-        <DateSelect
-          date={date}
-          onChange={setDate}
-          min={useMemo(() => new Date(), [])}
-        />
-        <HourSelectField
-          name="start"
-          day={date}
-          minHour={7}
-          maxHour={22}
-          minutesStep={30}
-          label="Start time"
-        />
-        <HourSelectField
-          name="end"
-          day={date}
-          minHour={7}
-          maxHour={22}
-          minutesStep={30}
-          label="End time"
-        />
-        <SelectField
-          name="teacherId"
-          label="Teacher"
-          options={personToOptions(teachers)}
-        />
-      </div>
+      <SelectField
+        name={"courtId"}
+        label={"Choose court"}
+        options={itemsToOptions(courts)}
+      />
+      <DateSelect
+        date={date}
+        onChange={setDate}
+        min={useMemo(() => new Date(addDays(date, 1)), [])}
+        max={useMemo(() => new Date(addDays(date, 15)), [])}
+      />
+      <HourSelectField
+        name="start"
+        day={date}
+        minHour={7}
+        maxHour={22}
+        minutesStep={30}
+        label="Start time"
+      />
+      <HourSelectField
+        name="end"
+        day={date}
+        minHour={7}
+        maxHour={22}
+        minutesStep={30}
+        label="End time"
+      />
+      <SelectField
+        name="teacherId"
+        label="Teacher"
+        options={personToOptions(teachers)}
+      />
     </Form>
   );
 };
--- src/frontend/shared/components/Form/Form.tsx
+++ src/frontend/shared/components/Form/Form.tsx
@@ -1,12 +1,11 @@
 import { Formik } from "formik";
-import { ReactElement } from "react";
+import { ReactNode } from "react";
 import { style } from "styles";
 import { isEntity } from "shared/utils";
-import { Grid } from "@mui/material";
 import { Actions } from "./Actions";
 
 interface Props<T extends object> {
-  children?: ReactElement;
+  children?: ReactNode;
   initialValues: T;
   onSubmit: (values: T) => Promise<void>;
   onRemove?: (values: T) => void;
@@ -21,18 +20,14 @@ export const Form = <T extends object>({
   <Formik {...props} validateOnMount>
     {(props) => (
       <form className={style("form")}>
-        <Grid container>
-          <Grid container>{children}</Grid>
-          <Grid container style={{ padding: 0 }}>
-            <Actions
-              onSubmit={async () => (await props.submitForm(), props.isValid)}
-              onRemove={
-                (isEntity(props.values) || undefined) &&
-                (async () => onRemove?.(props.values))
-              }
-            />
-          </Grid>
-        </Grid>
+        {children}
+        <Actions
+          onSubmit={async () => (await props.submitForm(), props.isValid)}
+          onRemove={
+            (isEntity(props.values) || undefined) &&
+            (async () => onRemove?.(props.values))
+          }
+        />
       </form>
     )}
   </Formik>
