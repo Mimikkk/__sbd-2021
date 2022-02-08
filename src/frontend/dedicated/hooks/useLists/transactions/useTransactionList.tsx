a0a8eed49a1a6c882396c433b534f7d0d599eae7
Hououin Kyouma <inwazzin@gmail.com>
2022-02-08 23:48:05 +0100

    transaction view

--- src/frontend/dedicated/hooks/useLists/transactions/useTransactionList.tsx
+++ src/frontend/dedicated/hooks/useLists/transactions/useTransactionList.tsx
@@ -1,19 +1,59 @@
 import { useList } from "shared/hooks";
-import { Client, Discount, Transaction } from '@models';
-import { clientService, discountService, transactionService } from '$/services';
-import { useEffect, useState } from 'react';
-import { uuid } from '@internal/types';
-import { keyBy } from 'lodash';
-import { getColumns } from './columns';
+import { Client, Discount, Price, Reservation, Transaction } from "@models";
+import {
+  clientService,
+  courtReservationService,
+  discountService,
+  itemReservationService,
+  priceService,
+  transactionService,
+} from "$/services";
+import { useEffect, useState } from "react";
+import { uuid } from "@internal/types";
+import { extend, keyBy } from "lodash";
+import { getColumns } from "./columns";
 
 export const useTransactionList = () => {
   const [Items, Context] = useList<Transaction.Row>(transactionService.readAll);
 
   const [discounts, setDiscounts] = useState<Record<uuid, Discount.Entity>>({});
-  useEffect(() => {discountService.readAll().then(({ items }) => setDiscounts(keyBy(items, "id")));}, []);
+  useEffect(() => {
+    discountService
+      .readAll()
+      .then(({ items }) => setDiscounts(keyBy(items, "id")));
+  }, []);
 
   const [clients, setClients] = useState<Record<uuid, Client.Entity>>({});
-  useEffect(() => {clientService.readAll().then(({ items }) => setClients(keyBy(items, "id")));}, []);
+  useEffect(() => {
+    clientService.readAll().then(({ items }) => setClients(keyBy(items, "id")));
+  }, []);
 
-  return [() => <Items columns={getColumns({clients, discounts})} pagination />, Context,] as const;
+  const [prices, setPrices] = useState<Record<uuid, Price.Entity>>({});
+  useEffect(() => {
+    priceService.readAll().then(({ items }) => setPrices(keyBy(items, "id")));
+  }, []);
+
+  const [reservations, setReservations] = useState<
+    Record<uuid, Reservation.Entity>
+  >({});
+  useEffect(() => {
+    Promise.all([
+      courtReservationService.readAll(),
+      itemReservationService.readAll(),
+    ]).then(([{ items: courts }, { items: items }]) => {
+      extend(reservations, keyBy(courts, "id"));
+      extend(reservations, keyBy(items, "id"));
+      setReservations(reservations);
+    });
+  }, []);
+
+  return [
+    () => (
+      <Items
+        columns={getColumns({ clients, discounts, prices, reservations })}
+        pagination
+      />
+    ),
+    Context,
+  ] as const;
 };
--- src/frontend/shared/components/List/components/cells/DateCell.ts
+++ src/frontend/shared/components/List/components/cells/DateCell.ts
@@ -1,5 +1,8 @@
 import { CellProps } from "react-table";
 import { format } from "date-fns";
 
+export const formatDate = (value: any) =>
+  format(new Date(value), "dd-MM-yyyy  HH:mm");
+
 export const DateCell = <T extends object>({ value }: CellProps<T>) =>
-  value && format(new Date(value), "dd-MM-yyyy  HH:mm");
+  value && formatDate(value);
